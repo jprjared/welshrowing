ea6a3b4eeac59ec42a6913126b90af5bb81650bb
James Jared <jaredj@cardiff.ac.uk>
2020-11-19 12:57:43 +0000

    Initial commit

---
+++ src/main/java/com/team1/welshrowing/WelshRowingApplication.java
@@ -0,0 +1,13 @@
+package com.team1.welshrowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelshRowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelshRowingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team1/welshrowing/WelshRowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.welshrowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelshRowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
