b57baa68cc36b0613f8c1a04b14e42577de98a13
c1847017 <yiallouridess@cardiff.ac.uk>
2020-11-24 12:50:35 +0000

    added applicant tests

---
+++ src/test/java/com/team1/welshrowing/ApplicantTests.java
@@ -0,0 +1,46 @@
+package com.team1.welshrowing;
+
+import com.team1.welshrowing.domain.Applicant;
+import com.team1.welshrowing.domain.Athlete;
+import com.team1.welshrowing.repository.ApplicantRepoJPA;
+import com.team1.welshrowing.repository.AthleteRepoJPA;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class ApplicantTests {
+
+    @Autowired
+    private ApplicantRepoJPA applicantRepo;
+
+    //    Allows to use EntityManager in tests
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void CreateApplicant1() {
+        Applicant newApplicant = new Applicant();
+        newApplicant.setUser_name("Sotiris");
+        newApplicant.setEmail("jacob@gmail.com");
+        newApplicant.setAll_other_info("No info");
+        newApplicant.setApplication_situation("Pending");
+        newApplicant.setCoach("Jenny");
+
+        applicantRepo.save(newApplicant);
+
+        Applicant savedApplicant = applicantRepo.save(newApplicant);
+
+        Applicant existApplicant = entityManager.find(Applicant.class, savedApplicant.getApplicant_id());
+
+        assertThat(existApplicant.getApplicant_id()).isEqualTo(newApplicant.getApplicant_id());
+
+    }
+}
