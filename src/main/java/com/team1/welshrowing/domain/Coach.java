9b19d6c0410a1c26997615d10cecd0d877e0327c
James Jared <jaredj@cardiff.ac.uk>
2020-11-27 14:17:14 +0000

    test

--- src/main/java/com/team1/welshrowing/domain/Coach.java
+++ src/main/java/com/team1/welshrowing/domain/Coach.java
@@ -1,17 +1,17 @@
-package com.team1.welshrowing.domain;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-//@Entity
-public class Coach {
-
-
-}
+//package com.team1.welshrowing.domain;
+//
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+////@Entity
+//public class Coach {
+//
+//
+//}
