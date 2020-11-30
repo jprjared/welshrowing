ddd7b7c20c7f19cccc806bfde2aeb30a25c18338
c1847017 <yiallouridess@cardiff.ac.uk>
2020-11-30 18:01:21 +0000

    Update ApplicantForm.java class with new fields

--- src/main/java/com/team1/welshrowing/web/ApplicantForm.java
+++ src/main/java/com/team1/welshrowing/web/ApplicantForm.java
@@ -9,15 +9,85 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ApplicantForm {
 
-    private Long applicant_id;
+    private Long applicantId;
 
+    /**
+     * The user name of the applicant.
+     */
     private String userName;
 
-    private String email;
+    /**
+     * The first name of the applicant.
+     */
+    private String firstName;
 
-    private String all_other_info;
+    /**
+     * The last name of the applicant.
+     */
+    private String lastName;
 
+    /**
+     * The phone number of the applicant.
+     */
+    private String phoneNumber;
+
+    /**
+     * The address of the applicant.
+     */
+    private String address;
+
+    /**
+     * The postcode of the applicant.
+     */
+    private String postcode;
+
+    /**
+     * The user name of the applicant.
+     */
+    private String college_university;
+
+    /**
+     * The date of birth of the applicant.
+     */
+    private String dob;
+
+    /**
+     * The height of the applicant.
+     */
+    private Double height;
+
+    /**
+     * The gender of the applicant.
+     */
+    private String gender;
+
+    /**
+     * The parent's email of the applicant.
+     */
+    private String parentEmail;
+
+    /**
+     * The passport of the applicant.
+     */
+    private String passportHolder;
+
+    /**
+     * Has the applicant been previously tested before.
+     */
+    private Boolean isPreviouslyTested = false;
+
+    /**
+     * From where the applicant have heard about the club.
+     */
+    private String whereDidHear;
+
+    /**
+     * The situation of the application of the applicant.
+     */
     private String application_situation;
 
+    /**
+     * The coach of the applicant.
+     */
     private String coach;
 }
