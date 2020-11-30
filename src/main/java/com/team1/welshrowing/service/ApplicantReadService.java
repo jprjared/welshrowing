3f8aa6015f3003b66a3ab2da31b83edc90d694a6
c1847017 <yiallouridess@cardiff.ac.uk>
2020-11-30 15:08:55 +0000

    Create ApplicantCreateService, ApplicantReadService classes implementing findByStatus method

---
+++ src/main/java/com/team1/welshrowing/service/ApplicantReadService.java
@@ -0,0 +1,24 @@
+package com.team1.welshrowing.service;
+
+import com.team1.welshrowing.domain.Applicant;
+import com.team1.welshrowing.repository.ApplicantRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ApplicantReadService {
+
+    private final ApplicantRepo repository;
+
+    @Autowired
+    public ApplicantReadService(ApplicantRepo repository) {
+        this.repository = repository;
+    }
+
+    public List<Applicant> findByStatus(String aStatus) {
+        return repository.ApplicantFindByStatus(aStatus);
+    }
+
+}
