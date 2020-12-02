d19e135eb341f7e2edbab368a0e6cab6bbe21211
Ryan Davies <daviesdr9@cardiff.ac.uk>
2020-12-02 20:37:09 +0000

    Added test for get applicant data

--- src/test/java/com/team1/welshrowing/ApplicantTests.java
+++ src/test/java/com/team1/welshrowing/ApplicantTests.java
@@ -2,52 +2,33 @@ package com.team1.welshrowing;
 
 import com.team1.welshrowing.domain.Applicant;
 import com.team1.welshrowing.repository.ApplicantRepoJPA;
+import com.team1.welshrowing.service.ApplicantCreateService;
+import com.team1.welshrowing.service.ApplicantReadService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(false)
+@SpringBootTest
 public class ApplicantTests {
 
     @Autowired
-    private ApplicantRepoJPA applicantRepo;
+    private ApplicantCreateService applicantCreateService;
 
-    //    Allows to use EntityManager in tests
     @Autowired
-    private TestEntityManager entityManager;
+    private ApplicantReadService applicantReadService;
 
     @Test
-    public void CreateApplicant1() {
-        Applicant newApplicant = new Applicant();
-        newApplicant.setUserName("Sotiris123");
-        newApplicant.setFirstName("Sotiris");
-        newApplicant.setLastName("Yiallourides");
-        newApplicant.setPhoneNumber("99355349");
-        newApplicant.setAddress("Agiou Genadiou");
-        newApplicant.setPostcode("cf 10 3fr");
-        newApplicant.setCollege_university("Cardiff University");
-        newApplicant.setDob("03/09/1932");
-        newApplicant.setHeight(1.23);
-        newApplicant.setGender("Male");
-        newApplicant.setParentEmail("dwadf@gmail.com");
-        newApplicant.setPassportHolder("No");
-        newApplicant.setWhereDidHear("No where");
-        newApplicant.setApplication_situation("Accepted");
-        newApplicant.setCoach("Emily");
-
-        applicantRepo.save(newApplicant);
-
-        Applicant savedApplicant = applicantRepo.save(newApplicant);
-
-        Applicant existApplicant = entityManager.find(Applicant.class, savedApplicant.getApplicantId());
-
-        assertThat(existApplicant.getApplicantId()).isEqualTo(newApplicant.getApplicantId());
+    public void create_an_applicant_and_find_them() {
+        Applicant theApplicant = new Applicant();
+        theApplicant.setApplicantId((long) 1);
+        applicantCreateService.addApplicant(theApplicant);
+        Assertions.assertEquals(theApplicant.getApplicantId(), applicantReadService.findById((long) 1).get().getApplicantId());
     }
 }
