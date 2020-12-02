34a7be4a3ae08c6ab3ef28bbb2e73e7f082f5b0a
Ryan Davies <daviesdr9@cardiff.ac.uk>
2020-12-02 19:46:24 +0000

    Fix merge issue

--- src/main/java/com/team1/welshrowing/web/CoachController.java
+++ src/main/java/com/team1/welshrowing/web/CoachController.java
@@ -1,11 +1,12 @@
-<<<<<<< src/main/java/com/team1/welshrowing/web/CoachController.java
 package com.team1.welshrowing.web;
 
 import com.cemiltokatli.passwordgenerate.Password;
 import com.cemiltokatli.passwordgenerate.PasswordType;
 import com.team1.welshrowing.domain.Applicant;
 import com.team1.welshrowing.domain.User;
+import com.team1.welshrowing.repository.ApplicantRepoJPA;
 import com.team1.welshrowing.service.ApplicantReadService;
+import com.team1.welshrowing.service.ApplicantUpdateService;
 import com.team1.welshrowing.service.UserCreateService;
 import com.team1.welshrowing.service.UserReadService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -123,65 +124,3 @@ public class CoachController {
     }
 
 }
-=======
-package com.team1.welshrowing.web;
-
-
-import com.team1.welshrowing.domain.Applicant;
-import com.team1.welshrowing.repository.ApplicantRepoJPA;;
-import com.team1.welshrowing.service.ApplicantReadService;
-import com.team1.welshrowing.service.ApplicantUpdateService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Optional;
-
-@Controller
-public class CoachController {
-
-    @Autowired
-    public ApplicantRepoJPA applicantRepo ;
-
-    @Autowired
-    private ApplicantUpdateService applicantUpdateService;
-
-    @Autowired
-    private ApplicantReadService applicantReadService;
-
-    /**
-     * GETs the athlete dashboard form.
-     */
-//    @GetMapping("/allApplicants")
-//    public String allApplicants() {
-//        return "applicantList";
-//    }
-
-
-    @GetMapping("/allApplicants")
-    public String getApplicant(Model model){
-
-        model.addAttribute("applicants", applicantRepo.findAll());
-        System.out.println(applicantRepo.findAll());
-        return "applicantList";
-    }
-
-    @PostMapping("/allApplicants/accept/{id}")
-    public String AcceptAnApplicant(@PathVariable Long id) {
-        Optional<Applicant> applicant = applicantReadService.findById(id);
-        applicantUpdateService.updateApplicantStatus(applicant.get(), "Accepted");
-        return "redirect:/allApplicants";
-    }
-
-    @PostMapping("/allApplicants/reject/{id}")
-    public String RejectAnApplicant(@PathVariable Long id) {
-        Optional<Applicant> applicant = applicantReadService.findById(id);
-        applicantUpdateService.updateApplicantStatus(applicant.get(), "Rejected");
-        return "redirect:/allApplicants";
-    }
-
-}
->>>>>>> src/main/java/com/team1/welshrowing/web/CoachController.java
