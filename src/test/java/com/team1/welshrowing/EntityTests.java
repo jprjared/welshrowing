82a252c8bd055d703482884d24cab065c4c4c9ba
Ryan Davies <daviesdr9@cardiff.ac.uk>
2020-12-03 13:55:14 +0000

    Fix tests

--- src/test/java/com/team1/welshrowing/EntityTests.java
+++ src/test/java/com/team1/welshrowing/EntityTests.java
@@ -32,7 +32,7 @@ public class EntityTests {
         newApplicant.setPhoneNumber("99355349");
         newApplicant.setAddress("Agiou Genadiou");
         newApplicant.setPostcode("cf 10 3fr");
-        newApplicant.setCollege_university("Cardiff University");
+        newApplicant.setCollegeOrUniversityName("Cardiff University");
         newApplicant.setDob("03/09/1932");
         newApplicant.setHeight(1.23);
         newApplicant.setGender("Male");
