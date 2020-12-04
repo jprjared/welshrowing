da337fbf0117c805a418e7e3ff928fd9c454a79e
c1847017 <yiallouridess@cardiff.ac.uk>
2020-12-04 19:53:46 +0000

    Create an ApplicantEmailService class, adding sendApplicantEmailStatus method

---
+++ src/main/java/com/team1/welshrowing/service/ApplicantEmailService.java
@@ -0,0 +1,22 @@
+package com.team1.welshrowing.service;
+
+import com.team1.welshrowing.domain.Applicant;
+import com.team1.welshrowing.domain.User;
+import com.team1.welshrowing.repository.ApplicantRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ApplicantEmailService {
+
+    private final ApplicantRepo repository;
+
+    public ApplicantEmailService(ApplicantRepo repository) {
+        this.repository = repository;
+    }
+
+    public void sendApplicantEmailStatus(Applicant applicant, String email) {
+        repository.sendEmailStatus(applicant,email);
+    }
+}
