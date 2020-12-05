8f575cb37aca29e8e2adf50934598635f1625525
James Jared <jaredj@cardiff.ac.uk>
2020-12-05 15:11:44 +0000

    commented out prompt

--- src/main/java/com/team1/welshrowing/web/ApplicantController.java
+++ src/main/java/com/team1/welshrowing/web/ApplicantController.java
@@ -1,13 +1,23 @@
 package com.team1.welshrowing.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicantController {
 
     @GetMapping("/applicant/dashboard")
-    public String applicantDashboard() {
+    public String applicantDashboard(Model model) {
+
+//        model.addAttribute("")
         return "applicant-dashboard";
     }
+
+//    @PostMapping("/application/dashboard/status")
+//    public String getStatus() {
+//
+//
+//    }
 }
