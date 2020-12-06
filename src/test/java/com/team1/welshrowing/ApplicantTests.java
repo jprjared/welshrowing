2223ffd8c1b8328d7c474e3d08243137ed9256f3
c1847017 <yiallouridess@cardiff.ac.uk>
2020-12-06 15:17:15 +0000

    Add Tests

--- src/test/java/com/team1/welshrowing/ApplicantTests.java
+++ src/test/java/com/team1/welshrowing/ApplicantTests.java
@@ -1,8 +1,10 @@
 package com.team1.welshrowing;
 
 import com.team1.welshrowing.domain.Applicant;
+import com.team1.welshrowing.domain.User;
 import com.team1.welshrowing.repository.ApplicantRepoJPA;
 import com.team1.welshrowing.service.ApplicantCreateService;
+import com.team1.welshrowing.service.ApplicantEmailService;
 import com.team1.welshrowing.service.ApplicantReadService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,7 +22,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -34,6 +38,9 @@ public class ApplicantTests {
 
     @Autowired
     private ApplicantReadService applicantReadService;
+
+    @Autowired
+    private ApplicantEmailService applicantEmailService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -60,5 +67,30 @@ public class ApplicantTests {
                 .andExpect(content().string(containsString(aGivenName)));
 
     }
+
+    @Test
+    @WithUserDetails("coach")
+    public void create_applicant_and_find_user_email() throws Exception {
+
+        Long aGivenId = 1L;
+        String aGivenEmail = "test@test.com";
+
+        Applicant theApplicant = new Applicant();
+        User theUser = new User();
+        theApplicant.setApplicantId(aGivenId);
+        theUser.setEmail(aGivenEmail);
+
+
+        // Check that the applicant exists and email matches with User table
+        Assertions.assertEquals(aGivenEmail,applicantReadService.findById(aGivenId).get().getUser().getEmail());
+
+        mockMvc
+                .perform(post("/coach/applicant/accept/" + aGivenId))
+                .andDo(print())
+                .andExpect(status().is(403));
+
+    }
+
+
 
 }
