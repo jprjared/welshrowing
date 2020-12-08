9adcf2bedbf911075f2b613711067df6bfbbd3d0
James Jared <jaredj@cardiff.ac.uk>
2020-12-08 14:20:11 +0000

    fixed repo class

--- src/main/java/com/team1/welshrowing/repository/ApplicantFormRepoJPA.java
+++ src/main/java/com/team1/welshrowing/repository/ApplicantFormRepoJPA.java
@@ -4,4 +4,5 @@ import com.team1.welshrowing.domain.ApplicationForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicantFormRepoJPA extends JpaRepository<ApplicationForm, Long> {
+    
 }
