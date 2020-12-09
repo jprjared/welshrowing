827fe5465870d10beed13436724c850ae26957d8
Ryan Davies <daviesdr9@cardiff.ac.uk>
2020-12-09 13:16:03 +0000

    Add set up for morning monitoring

---
+++ src/main/java/com/team1/welshrowing/domain/MorningMonitoring.java
@@ -0,0 +1,52 @@
+package com.team1.welshrowing.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
+import javax.validation.constraints.Size;
+import java.security.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class MorningMonitoring {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @OrderBy
+    private Long morningMonitoringId;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "userId")
+    private User userId;
+
+    @CreationTimestamp
+    private Timestamp dateTime;
+
+    private Integer wakingHeartRate;
+
+    private Integer standingHeartRate;
+
+    @Size(min = 1, max = 10, message = "Perceived shape must be between 1 and 10")
+    private Integer perceivedShape;
+
+    @Size(min = 1, max = 10, message = "Perceived mental state must be between 1 and 10")
+    private Integer perceivedMentalState;
+
+    private Integer sleepQuantity;
+
+    @Size(min = 1, max = 10, message = "Sleep quality must be between 1 and 10")
+    private Integer sleepQuality;
+
+}
---
+++ src/main/java/com/team1/welshrowing/web/AthleteController.java
@@ -0,0 +1,20 @@
+package com.team1.welshrowing.web;
+
+import com.team1.welshrowing.domain.MorningMonitoring;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AthleteController {
+
+    /**
+     * GETs the athlete details form.
+     */
+    @GetMapping("/athlete/morning-monitoring")
+    public String morningMonitoring(Model model) {
+        model.addAttribute("morningMonitoring", new MorningMonitoring());
+        return "morning-monitoring";
+    }
+
+}
