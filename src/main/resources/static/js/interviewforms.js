507aed5b7f248d4b579172893e312c4d081e37a4
Jac Phillipps <PhillippsJ@cardiff.ac.uk>
2020-12-15 19:47:27 +0000

    Added Homepage Content

--- src/main/resources/static/js/interviewforms.js
+++ src/main/resources/static/js/interviewforms.js
@@ -34,3 +34,31 @@ var notfuture = new Date(),
     minDate
 
 $('#timefromnow').prop('max', minDate);
+
+
+
+
+
+
+    // Adapted From https://www.w3schools.com/howto/howto_js_slideshow.asp
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
