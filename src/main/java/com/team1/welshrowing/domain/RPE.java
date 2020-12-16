51ca3e23be3ab4520dd2df0b211da0cdff87750d
c1847017 <yiallouridess@cardiff.ac.uk>
2020-12-16 02:17:52 +0000

    Add column transformer annotation

--- src/main/java/com/team1/welshrowing/domain/RPE.java
+++ src/main/java/com/team1/welshrowing/domain/RPE.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnTransformer;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -30,6 +31,7 @@ public class RPE {
 
     private String dateofTest;
 
+    @ColumnTransformer(read = "cast(aes_decrypt(typeof_session, 'password') as char(255))")
     private String typeofSession;
 
     private Integer rpe;
